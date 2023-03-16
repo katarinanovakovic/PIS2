a397d24b8e3cc594079d4a63cf9a213fcdd3334d
Katarina <knovak00@fesb.hr>
2023-03-16 17:28:25 +0100

    Promjena

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Katarina
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
