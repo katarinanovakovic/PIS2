57c5ef644eb0fa35ff103f751fd6f8af504587a2
lpuaca00 <92399417+lpuaca00@users.noreply.github.com>
2023-03-16 17:45:10 +0100

    Update Program.cs
    komentar za 17b

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Katarina
         {
             Console.WriteLine("Hello World2!");
             Console.WriteLine("Novo");
-            Console.WriteLine("lucijana je najbolja");
+            Console.WriteLine("lucijana je najbolja he he");
         }
     }
 }
